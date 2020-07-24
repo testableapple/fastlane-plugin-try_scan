63023b0a3f3a346c084e5f7eae8bc6692a9ca1c8
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2020-07-24 15:45:18 +0100

    Implement gitleaks and update check_changes script

--- .github/scripts/check_changes.rb
+++ .github/scripts/check_changes.rb
@@ -2,9 +2,14 @@ require 'json'
 
 raise "Projects folder names have to be provided to trigger the tests" if ENV['TRIGGERS'].nil?
 
-required_projects = ENV['TRIGGERS'].delete(' ').delete('/').split(',')
+required_projects = ENV['TRIGGERS'].split(',').map {|project| project.strip }
+except_projects = ENV['EXCLUDE_TRIGGERS'].nil? ? [] : ENV['EXCLUDE_TRIGGERS'].split(',').map {|project| project.strip }
 response = JSON.parse(`curl -s -H "authorization: Bearer #{ENV['GITHUB_TOKEN']}" -X GET -G #{ENV['PULL_REQUEST']}/files`)
 changed_files = response.map { |file| file['filename'] }
-changes = changed_files.select { |path| required_projects.any? { |project| path.include?("#{project}/") } }.size
+changed_files.select! do |path|
+  required_projects.any? do |required_project|
+    path.include?("#{required_project}/") && except_projects.none? { |excess_project| path.include?("#{excess_project}/") }
+  end
+end
 
-puts changes
+puts changed_files.size
